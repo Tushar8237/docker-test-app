25bef3552ae0d7d6e41cf3661c8855b2a547110c
Tushar Suryawanshi <tushar.823737@gmail.com>
2025-01-09 16:18:28 +0530

    project components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-     docker practice project
+      <Home />
+      <ContactUs />
+      <AboutUs />
     </div>
   );
 }
