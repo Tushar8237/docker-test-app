e2d0ba934808b7d40c52e08236a33678f64f24bd
Tushar Suryawanshi <tushar.823737@gmail.com>
2025-01-10 15:12:02 +0530

    stripe payment

--- src/components/stripe/CheckoutForm.jsx
+++ src/components/stripe/CheckoutForm.jsx
@@ -1,28 +1,53 @@
 import React, { useState } from 'react'
-import { PaymentElement, LinkAuthenticationElement} from '@stripe/react-stripe-js'
+import { PaymentElement, LinkAuthenticationElement } from '@stripe/react-stripe-js'
 import { useStripe, useElements } from '@stripe/stripe-js'
 
 function CheckoutForm() {
-  const stripe = useStripe();
-  const element = useElements();
-  const [message, setMessage] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+    const stripe = useStripe();
+    const elements = useElements();
+    const [message, setMessage] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    if (!stripe || !element) {
-      // Stripe.js has not yet load
-      // Make sure to disable form submission until Stripe.js has load
-      return;
+        if (!stripe || !elements) {
+            // Stripe.js has not yet load
+            // Make sure to disable form submission until Stripe.js has load
+            return;
+        }
+        setIsLoading(true)
+
+        const { error } = await stripe.confirmPayment({
+            elements,
+            confirmParams: {
+                return_url: `${window.location.origin}/completion`,
+            },
+        });
+
+        // This point will only be reached if there is an immediate error w
+        // confirming the payment. Otherwise, your customer will be redirect to return_url.
+        // For some payment method like ideal, be redirect to an intermediate site first to authorize the pay redirected to the return _url
+        if (error.type === "card_error" || error.type === "validation_error") {
+            setMessage(error.message)
+        } else {
+            setMessage("An unexpected error occur");
+        }
+
+        setIsLoading(false)
     }
-    setIsLoading(true)
-  }
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <form id='payment-form' onSubmit={handleSubmit}>
+            <LinkAuthenticationElement id="link-authentication-element" />
+            <PaymentElement id="payment-element" />
+            <button disabled={isLoading || !stripe || !elements} id="submit">
+                <span id="button-text">
+                    {isLoading ? <div className='spinner' id='spinner'></div> : "Pay now"}
+                </span>
+            </button>
+            {message && <div id="payment-message">{message}</div>}
+        </form>
+    )
 }
 
 export default CheckoutForm
---
+++ src/components/stripe/Completion.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
+function Completion(props) {
+    const [ messageBody, setMessageBody] = useState('');
+    const { stripePromise } = props
+
+    useEffect(() => {
+        if (!stripePromise) return;
+
+        stripePromise.then(async (stripe) => {
+            const url = new URL(window.location);
+            const clientSecret = url.searchParams.get('payment_intent_client_secret');
+            const { error, paymentIntent } = await stripe.retrievePayment (clientSecret);
+
+            setMessageBody(error ? `> ${error.message}` : (
+                <>&gt; Payment {paymentIntent.status} : <a href={`https://dashboard.strpe.com/test/payment/${paymentIntent.id}`} target='_blank' rel="noreferrer">{paymentIntent.id}</a></>
+            ))
+        })
+    }, [stripePromise]);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Completion
